740169c6a89a93bd94e728096bc1f4c08a939845
김중현 <110381888+gotkagovkfl@users.noreply.github.com>
2024-08-06 13:49:35 +0900

    무기슬롯 분리
    아직 애니메이션적용은 미완성

--- Assets/Scripts/@@TEST/TestEquipment.cs
+++ Assets/Scripts/@@TEST/TestEquipment.cs
@@ -1,20 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Animations.Rigging;
 using ULT;
+using System.Collections.Generic;
 using UnityEngine.Rendering;
-using UnityEditor.Animations;
-using UnityEngine.Profiling;
-using System;
+
+
+public enum WeaponSlot
+{
+    Primary,
+    Secondary,
+    Melee,
+    Support
+}
+
+
 
 public class TestEquipment : MonoBehaviour
 {
     [SerializeField] UltimatePlayerInput playerInput;
 
 
-    [SerializeField]  TestWeapon currWeapon;
-    [SerializeField] Transform weaponParent;
+    [SerializeField] Transform weaponSlot_primary;
+    [SerializeField] Transform weaponSlot_secondary;
+    
+    //
+    Dictionary<WeaponSlot, Transform> weaponSlots;      // 무기를 장착할 위치
+    Dictionary<WeaponSlot,TestWeapon> equippedWeapon = new(){ {WeaponSlot.Primary, null}, {WeaponSlot.Secondary, null}, {WeaponSlot.Melee, null} };
+    [SerializeField]  TestWeapon holdingWeapon;
+
 
     // [SerializeField] Rig handIK;
     // [SerializeField] Transform t_rHandGrip;
@@ -29,31 +41,36 @@ public class TestEquipment : MonoBehaviour
     [SerializeField] GameObject prefab_testPistol;
 
 
+    //======================================================================================================================
     void Start()
     {
         playerInput = GetComponent<UltimatePlayerInput>();
-        var weapon = GetComponentInChildren<TestWeapon>();
-        if (weapon)
-        {
-            Equip(weapon);
-        }
-        else
-        {
-            UnArm();
-        }
+    
+        // 변수는
+        weaponSlots = new(){ {WeaponSlot.Primary, weaponSlot_primary}, {WeaponSlot.Secondary, weaponSlot_secondary}, {WeaponSlot.Melee, null} };
+
+
+        // ------- 기본 무기 장착 ------------
+        TestWeapon w1= Instantiate(prefab_testRifle).GetComponent<TestWeapon>();
+        Equip(WeaponSlot.Primary, w1);
+        
+        TestWeapon w2= Instantiate(prefab_testPistol).GetComponent<TestWeapon>();
+        Equip(WeaponSlot.Secondary, w2);
+
+    
+        // 처음엔 무장 X - 나중엔 
+        Hold(WeaponSlot.Primary);
     }
 
     void Update()
     {
         if (playerInput.weaponSelect_main)
         {
-            TestWeapon w= Instantiate(prefab_testRifle).GetComponent<TestWeapon>();
-            Equip(w);
+            Hold(WeaponSlot.Primary);
         }
         else if (playerInput.weaponSelect_secondary)
         {
-            TestWeapon w= Instantiate(prefab_testPistol).GetComponent<TestWeapon>();
-            Equip(w);
+            Hold(WeaponSlot.Secondary);
         }
         else if (playerInput.weaponSelect_melee)
         {
@@ -62,7 +79,7 @@ public class TestEquipment : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.X))
         {
-            Debug.Log($"Holster {currWeapon?.gameObject.name}");
+            Debug.Log($"Holster {holdingWeapon?.gameObject.name}");
             UnArm();
         }
 
@@ -70,48 +87,85 @@ public class TestEquipment : MonoBehaviour
     }
 
 
-    void Equip(TestWeapon weapon)
+    //=======================================================================================
+
+    /// <summary>
+    /// weaponSlot 에 weapon을 장착한다. 
+    /// </summary>
+    /// <param name="weaponSlot"></param>
+    /// <param name="weapon"></param>
+    void Equip(WeaponSlot weaponSlot, TestWeapon weapon)
     {
+        // 기존 무기 파괴
+        UnEquip(weaponSlot);
+
+        // 새 무기 장착
+        equippedWeapon[weaponSlot] = weapon;
+        Transform t_weaponSlot = weaponSlots[weaponSlot];
+        weapon.transform.SetParent(t_weaponSlot,false);
+    }
+
+    /// <summary>
+    ///  기존무기파괴
+    /// </summary>
+    /// <param name="weapon"></param>
+    void UnEquip(WeaponSlot weaponSlot)
+    {
+        TestWeapon currWeapon = equippedWeapon[weaponSlot];
         if (currWeapon)
         {
             Destroy(currWeapon.gameObject);
         }
+    }
+
+
+    /// <summary>
+    /// 해당 무기를 파지한다. 
+    /// </summary>
+    /// <param name="weaponSlot"></param>
+    void Hold(WeaponSlot weaponSlot)
+    {
+        TestWeapon weapon = equippedWeapon[weaponSlot];
+        Debug.Log(weapon);
+        holdingWeapon = weapon;
+
         holster = false;
 
-        currWeapon = weapon;
-    
-        currWeapon.transform.SetParent(weaponParent,false);
-
         animator.SetBool(hash_holster, holster);            // 무기를 장착하면 애니메이션 파라미터도 변경시킴   
-        animator.Play($"Equip_{currWeapon.type}");
+        animator.Play($"Hold_{holdingWeapon.type}");
 
-        Debug.Log($"무기장착 {weapon.gameObject.name}_{currWeapon.type}");
+        Debug.Log($"무기장착 {weapon.gameObject.name}_{holdingWeapon.type}");
+
     }
 
-    void UnEquip()
-    {
-        if (currWeapon)
-        {
-            Destroy(currWeapon.gameObject);
-        }
-    }
-
+    /// <summary>
+    ///  해당 무기를 보관한다.
+    /// </summary>
     void Holster()
     {
         holster = !holster;
         animator.SetBool(hash_holster, holster);        
     }
 
+    /// <summary>
+    /// 무장 해제한다. 
+    /// </summary>
     void UnArm()
     {
-        if (currWeapon)
-        {
-            Destroy(currWeapon.gameObject);
-        }
-        
         animator.Play("Unarmed");        
     }
     //===========================================
+
+
+
+
+
+
+
+
+
+
+
 
 
     //===============================================================
